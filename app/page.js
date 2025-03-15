9a67033bb3dffa30a332f2c753b46113ff8e4e4f
Kalama Ayubu <kalamayeng@gmail.com>
2025-03-15 18:47:49 +0300

    Refactor: Style of the footer and 'How does AttendMe work' section

--- app/page.js
+++ app/page.js
@@ -22,21 +22,27 @@ export default function NotificationSetup() {
         <div className="w-[85%] m-auto">
           <Hero/>
             <div className="flex flex-col lg:flex-row w-full gap-8 items-center">
-              <div className=" m-auto w-full md:max-w-[450px] rounded-lg">
+              <div className="m-auto w-full md:max-w-[450px] rounded-lg flex flex-col items-center">
+                <h1 className="text-center text-4xl flex lg:hidden mt-14 mb-4">How does AttendMe work</h1>
                 <Image src={'/assets/howItWorks.webp'} width={500} height={500} alt="Image" className="w-full object-cover rounded-xl"/>
-                <Link href={"/"} className="mt-14">
+                <Link href={"/"} className="mt-4 self-start hidden lg:flex">
                   <button className="bg-gradient-to-br from-blue-700 from-20% to bg-purple-600 to-90% text-white rounded-md px-4 py-2 hover:bg-gradient-to-tl hover:from-blue-700 hover:to-purple-600 mt-8 m-auto">
                     Start Tracking Attendance
                   </button>
                 </Link>
               </div>
 
-              <div>
-                <div className="">
-                  <h1 className="text-center text-4xl">How it works</h1>
-                  <HowItWorks/>
+                <div className="flex flex-col">
+                  <h1 className="text-center text-4xl hidden lg:flex">How does AttendMe work?</h1>
+                  <div className="">
+                    <HowItWorks/>
+                    <Link href={"/"} className="mt-4 flex lg:hidden">
+                      <button className="bg-gradient-to-br from-blue-700 from-20% to bg-purple-600 to-90% text-white rounded-md px-4 py-2 hover:bg-gradient-to-tl hover:from-blue-700 hover:to-purple-600 mt-8 m-auto">
+                        Start Tracking Attendance
+                      </button>
+                    </Link>
+                  </div>
                 </div>
-              </div>
             </div>
         </div>
         <Footer/>
--- components/general/Footer.jsx
+++ components/general/Footer.jsx
@@ -2,7 +2,7 @@ import SocialLinks from "./SocialContacts";
 
 const Footer = () => {
   return (
-    <footer className="w-full mt-20 bg-gray-900 bg-opacity-10">
+    <footer className="w-full mt-20">
       <div></div>
       <div className="h-[2px] w-full bg-gradient-to-r from-blue-600 to-pink-500" />
       <div className="mt-1 flex justify-between items-center px-4">
